95ed79bd21e181d9f48a9c6abc7b988fe7a360ac
Aegean <51271714+aegeanthompson@users.noreply.github.com>
2019-12-06 15:46:06 -0600

    Groceries Solution

---
+++ unit_4/react_groceries/app.js
@@ -0,0 +1,130 @@
+
+class App extends React.Component {
+  constructor (props){
+    super(props)
+    this.state = {
+      groceries: groceries,
+      item: '',
+      brand: '',
+      units: '',
+      quantity: 0,
+      isPurchased: true
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.togglePurchased = this.togglePurchased.bind(this)
+  }
+  handleChange(event) {
+    this.setState({[event.target.id]: event.target.value})
+  }
+  handleSubmit(event) {
+    console.log("handle submit activated");
+    event.preventDefault()
+    const newItem = {
+      item: this.state.item,
+      brand: this.state.brand,
+      units: this.state.units,
+      quantity: this.state.quantity,
+      isPurchased: false
+    }
+    this.setState({
+      groceries: [ newItem, ...groceries ],
+      item: '',
+      brand: '',
+      units: '',
+      quantity: 0,
+      isPurchased: false
+    })
+    console.log(this.state.groceries);
+  }
+
+  togglePurchased (item) {
+    item.isPurchased = !item.isPurchased;
+    this.setState({
+      isPurchased: this.state.isPurchased
+    })
+    console.log(item.isPurchased);
+  }
+
+  render () {
+    return (
+      <div>
+        <h1 class="title">My Groceries</h1>
+        <h4 class="addNew">ADD NEW ITEM</h4>
+      <div class="items">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="item"></label>
+          <h5>Item: </h5>
+          <input
+            type="text"
+            value={this.state.item}
+            onChange={this.handleChange}
+            id="item"
+          />
+          <br />
+          <label htmlFor="brand"></label>
+          <h5>Brand: </h5>
+          <input
+            type="text"
+            value={this.state.brand}
+            onChange={this.handleChange}
+            id="brand"
+          />
+          <br />
+          <label htmlFor="units"></label>
+          <h5>Units: </h5>
+          <input
+            type="text"
+            value={this.state.units}
+            onChange={this.handleChange}
+            id="units"
+          />
+          <br />
+          <label htmlFor="quantity"></label>
+          <h5>Quantity: </h5>
+          <input
+              type="text"
+              value={this.state.quantity}
+              onChange={this.handleChange}
+              id="quantity"
+          />
+          <br />
+          <input class="addButton" type="submit" value="Add" />
+        </form>
+          <div>
+            <div class="preview">
+              <h4>Preview New Item</h4>
+              <h3>{this.state.item}</h3>
+              <h4>{this.state.brand}</h4>
+              <h5>{this.state.units}</h5>
+              <h5>{this.state.quantity}</h5>
+            </div>
+          </div>
+        </div>
+        <div class="toPurchase">
+          <h2>To Purchase:</h2>
+          <ul>
+            {this.state.groceries.map(item => {
+              if (!item.isPurchased){
+              return (
+                <li>
+                  {item.item} {item.brand} {item.units} {item.quantity}
+                  <br />
+                  <button onClick={this.togglePurchased}>Remove</button>
+                </li>
+              )
+            }
+          }
+        )}
+          </ul>
+      </div>
+    </div>
+    )
+  }
+}
+
+
+  ReactDOM.render(
+    <App />,
+    document.querySelector('.container')
+  )
---
+++ unit_4/react_groceries/data.js
@@ -0,0 +1,24 @@
+const groceries = [
+  {
+    item: 'Chicken',
+    brand: 'HEB',
+    units: '2lbs',
+    quantity: 1,
+    isPurchased: false
+  },
+  {
+    item: 'Gravy',
+    brand: 'McCormick',
+    units: '2 pack',
+    quantity: 1,
+    isPurchased: false
+  },
+  {
+    item: 'Rice',
+    brand: 'Mahatma',
+    units: '1lb',
+    quantity: 2,
+    isPurchased: false
+  },
+
+]
